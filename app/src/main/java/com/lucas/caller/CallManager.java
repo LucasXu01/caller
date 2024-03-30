7788017e6c3598bc081d3449bb53177bf33e8628
xujinjin <xujinjin@58.com>
2024-03-30 21:41:35 +0800

    打电话

---
+++ app/src/main/java/com/lucas/caller/CallManager.java
@@ -0,0 +1,32 @@
+package com.lucas.caller;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.blankj.utilcode.util.LogUtils;
+
+import java.util.List;
+
+public class CallManager {
+
+
+
+
+}
+
+
+
--- app/src/main/java/com/lucas/caller/MainActivity.java
+++ app/src/main/java/com/lucas/caller/MainActivity.java
@@ -1,26 +1,30 @@
 package com.lucas.caller;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.telecom.PhoneAccountHandle;
+import android.telecom.TelecomManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.blankj.utilcode.util.FileUtils;
 import com.blankj.utilcode.util.UriUtils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,25 +38,60 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_FILE_REQUEST_CODE = 1;
     private static final int STORAGE_PERMISSION_REQUEST_CODE = 100;
+    //拨号请求码
+    public static final int REQUEST_CALL_PERMISSION = 10111;
 
+
+    private Button button_interval_duration;
+    private Button button_start_auto_dial;
+    private Button button;
+    private ImageView img;
     private RecyclerView recyclerView;
     private PhoneAdapter adapter;
     private List<PhoneBean> phoneList = new ArrayList<>();
+    private int 间隔时间 = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button_start_auto_dial = findViewById(R.id.button_start_auto_dial);
+        button_interval_duration = findViewById(R.id.button_interval_duration);
         // 点击按钮触发文件选择操作
-        findViewById(R.id.button_select_file).setOnClickListener(view -> pickExcelFile());
+        button = findViewById(R.id.button_select_file);
+        button.setOnClickListener(view -> pickExcelFile());
+        img = findViewById(R.id.img);
+        img.setOnClickListener(view -> {
+            CustomDialog dialog = new CustomDialog(this);
+            dialog.show();
+        });
+        button_interval_duration.setOnClickListener(v -> {
+            IntervalInputDialog dialog = new IntervalInputDialog(this);
+            dialog.setOnIntervalSetListener(new IntervalInputDialog.OnIntervalSetListener() {
+                @Override
+                public void onIntervalSet(int interval) {
+                    // 在这里处理用户输入的间隔参数
+                    // interval 即为用户输入的间隔参数
+                    间隔时间 = interval;
+//                    ToastUtils.showShort("已设置间隔为" + interval + "秒");
+                    button_interval_duration.setText("间隔时长（" + interval + "s）");
+
+                }
+            });
+            dialog.show();
+
+        });
+
+        button_start_auto_dial.setOnClickListener(v -> {
+//            CallManager.callPhone(this, "17136867768", 1);
+            callPhone("17136867768", 1);
+        });
+
 
         // 检查权限
         checkStoragePermission();
-
-
-
-
+        checkReadPermission2(Manifest.permission.CALL_PHONE, REQUEST_CALL_PERMISSION);
 
 
         recyclerView = findViewById(R.id.recyclerView);
@@ -80,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     // 根据isCalled字段更新列表显示
     private void updateList(boolean isCalled) {
         List<PhoneBean> filteredList = new ArrayList<>();
@@ -104,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
                             != PackageManager.PERMISSION_GRANTED) {
                 // 如果权限没有被授予，请求权限
                 ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CALL_PHONE},
                         STORAGE_PERMISSION_REQUEST_CODE);
             } else {
                 // 权限已经被授予
@@ -114,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
             // 如果Android版本在Marshmallow以下，无需请求权限
             // 在这里处理您的逻辑
         }
+
     }
 
 
@@ -172,10 +211,85 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
             rawList.clear();
 
+
         } catch (IOException | BiffException e) {
             e.printStackTrace();
         }
     }
 
+
+    //打电话申请权限，
+    public boolean checkReadPermission2(String string_permission, int request_code) {
+        boolean flag = false;
+//已有权限
+        if (ContextCompat.checkSelfPermission(this, string_permission) == PackageManager.PERMISSION_GRANTED) {
+            flag = true;
+        } else {
+//申请权限
+            ActivityCompat.requestPermissions(this, new String[]{string_permission}, request_code);
+        }
+        return flag;
+    }
+
+
+    private final static String simSlotName[] = {
+            "extra_asus_dial_use_dualsim",
+            "com.android.phone.extra.slot",
+            "slot",
+            "simslot",
+            "sim_slot",
+            "subscription",
+            "Subscription",
+            "phone",
+            "com.android.phone.DialingMode",
+            "simSlot",
+            "slot_id",
+            "simId",
+            "simnum",
+            "phone_type",
+            "slotId",
+            "slotIdx"};
+
+
+    /**
+     * 拨打电话（拨号权限自行处理）
+     * @param phoneNum ：目标手机号
+
+     * @param simIndex ：sim卡的位置 0代表sim卡1，1代表sim卡2
+
+     */
+
+    public void callPhone(String phoneNum, int simIndex) {
+
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:17136867768"));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("com.android.phone.force.slot", true);
+        intent.putExtra("Cdma_Supp", true);
+        //Add all slots here, according to device.. (different device require different key so put all together)
+        for (String s : simSlotName)
+            intent.putExtra(s, 1); //0 or 1 according to sim.......
+        //works only for API >= 21
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                TelecomManager telecomManager = (TelecomManager) getSystemService(Context.TELECOM_SERVICE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                List<PhoneAccountHandle> phoneAccountHandleList = telecomManager.getCallCapablePhoneAccounts();
+                intent.putExtra("android.telecom.extra.PHONE_ACCOUNT_HANDLE", phoneAccountHandleList.get(1));
+            } catch (Exception e) {
+                e.printStackTrace();
+                //writeLog("No Sim card? at slot " + simNumber+"\n\n"+e.getMessage(), this);
+            }
+        }
+        startActivity(intent);
+    }
 
 }
